def959487e16a5f8d0c431cede0e966f6f0b6eca
Vikas Kaler <Vikas.kaler@mobilelive.ca>
2021-01-08 13:32:38 -0500

    chore: added initial configuration chore: added routing and lazy loading chore: added navigation component chore: added routes for all the possible pagges [home, orders, products,cart]

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,28 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule)
+  },
+  {
+    path: 'orders',
+    pathMatch: 'full',
+    loadChildren: () => import('./pages/orders/orders.module').then(m => m.OrdersModule)
+  },
+  {
+    path: 'products',
+    pathMatch: 'full',
+    loadChildren: () => import('./pages/products/products.module').then(m => m.ProductsModule)
+  },
+  {
+    path: 'cart',
+    pathMatch: 'full',
+    loadChildren: () => import('./pages/cart/cart.module').then(m => m.CartModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
